697741becbbada923fac803ac6e32b8d7e84bf22
janith nirmal <113751270+janithnirmal@users.noreply.github.com>
2025-05-07 09:24:17 +0530

    release v.0.2.3
    Bug Fixed and other updates
    - contact form integrated
    - social icon update
    - minor bug fixes

--- public/build/assets/input-error-D8_9L9lX.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./app-7aIYh_Ic.js";import{c as o}from"./button-DivPSDMD.js";function m({message:r,className:t="",...e}){return r?n.jsx("p",{...e,className:o("text-sm text-red-600 dark:text-red-400",t),children:r}):null}export{m as I};
