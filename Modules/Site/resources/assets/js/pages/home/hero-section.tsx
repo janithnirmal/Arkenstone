d6bc4c01937cdf45a638acd8326780a29529c909
irushi <irushiweerasinghe716@gmail.com>
2025-10-24 19:17:33 +0530

    home sections

---
+++ Modules/Site/resources/assets/js/pages/home/hero-section.tsx
@@ -0,0 +1,30 @@
+import Search from '@site/components/search/search';
+import TextAdvantPro from '@site/components/text/text-AdvantPro';
+import { Button } from '@site/components/button/default-button';
+import { ArrowRight } from 'lucide-react';
+import HeroImgContainer from '@site/components/heroImgContainter/hero-img-container';
+
+export default function HeroSection() {
+    return (
+        <div className="container mx-auto flex lg:min-h-[80vh] flex-col px-5 py-5 bg-white">
+            <div className='flex w-full'>
+                <div className='lg:w-1/2 flex-1 h-full lg:min-h-[80vh] px-10 space-y-10 flex flex-col'>
+                    <Search />
+                    <div className='lg:w-1/2'>
+                        <TextAdvantPro size="4xl" weight="bold" className="lg:text-7xl">NEW COLLECTION</TextAdvantPro>
+                    </div>
+                    <div>
+                        <img src="/storage/hero/MOM TEX.png" alt="Hero Image" className='lg:w-3/4' />
+                        <img src="/storage/hero/MOM MAKES YOUR LIFE WOW.png" alt="Hero Image" className='lg:w-3/4 mt-5' />
+                    </div>
+                    <div className='flex-1 flex items-center '>
+                        <Button size={"large"} rightIcon={<ArrowRight />} className='gap-30'>Button</Button>
+                    </div>
+                </div>
+                <div className='lg:w-1/2 px-5 space-y-10'>
+                    <HeroImgContainer imagePath="/storage/hero/new_collection/photo.svg" />
+                </div>
+            </div>
+        </div>
+    );
+}
